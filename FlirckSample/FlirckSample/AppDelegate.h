bde9af3a929de51b63dd8153cb46bf974575cd8e
Wojciech Charysz <wojciech.charysz@match2blue.com>
2015-05-11 10:02:24 +0200

    Initial App project

---
+++ FlirckSample/FlirckSample/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FlirckSample
+//
+//  Created by Wojtek Charysz on 11.05.15.
+//  Copyright (c) 2015 Wojciech Charysz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ FlirckSample/FlirckSample/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FlirckSample
+//
+//  Created by Wojtek Charysz on 11.05.15.
+//  Copyright (c) 2015 Wojciech Charysz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
